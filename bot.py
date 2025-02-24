e1a95fa795a280d3f2b26aba05619336ea1348d1
Muhammad Rierie <168540651+mhmmdrierie@users.noreply.github.com>
2025-02-24 10:39:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from threading import Lock
 import time
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-BOT_TOKEN = "TOKEN AQUI"
-ADMIN_ID = 7178876305
+BOT_TOKEN = "8067725084:AAHUr1qn-A8znbaEqUCS8zRYa60hegihvMo"
+ADMIN_ID = 6851490287
 START_PY_PATH = "/workspaces/MHDDoS/start.py"
 
 bot = telebot.TeleBot(BOT_TOKEN)
